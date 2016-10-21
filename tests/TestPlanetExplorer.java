dee6dd5ec5b708d879e0067c378b49226825c721
somename <someemail>
2016-10-21 14:50:35 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,26 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	// PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")
+	public void test_PlanetExplorer_constructor_2_obstacles() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		int NumberOfExplorer = explorer.obstacle.size();
+		assertEquals(2,NumberOfExplorer);
+	}
+	
+	@Test
+	// PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")
+	public void test_PlanetExplorer_constructor_5_5_explorer_true() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		boolean check = explorer.obstacle.containsKey("(5,5)");
+		assertTrue(check);
+	}
+	
+	@Test
+	// PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")
+	public void test_PlanetExporer_constructor_7_8_explorer_true() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		boolean check = explorer.obstacle.containsKey("(7,8)");
+		assertTrue(check);
 	}
 }
