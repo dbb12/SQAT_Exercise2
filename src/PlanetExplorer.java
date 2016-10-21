9156a8c7450c2877c5c6ff0e9c077f4ca9ee0103
somename <someemail>
2016-10-21 15:43:59 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -16,8 +16,7 @@ public class PlanetExplorer {
 	 *  
 		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
-		 
-	 */
+	 */                                     
 		this.x = x;
 		this.y = y;
 		int LeftPosition = 0,   // LeftPosition:   record the index of '(' in the String - obstacles
@@ -55,7 +54,6 @@ public class PlanetExplorer {
 	}
 	
 	public String executeCommand(String command){
-		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
 		 * Example: 
 		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
@@ -65,7 +63,15 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		int pos_x = 0, pos_y = 0; // coordinates of the position of the explorer
+		String facing = "N";
+		String result = null;
+		String obs = null;
 		
-		return null;
+		
+		
+		
+		result = "(" + String.valueOf(pos_x) + "," + String.valueOf(pos_y) + facing + ")" + obs ;
+		return result;
 	}
 }
